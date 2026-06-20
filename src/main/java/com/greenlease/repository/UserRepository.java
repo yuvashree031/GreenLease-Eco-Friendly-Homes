9f09ad0899ceea3169dd62b575fad88c848de994
Yuvashree R <yuvashree.ra05@gmail.com>
2026-06-20 21:31:49 +0530

    Update UserRepository.java

--- src/main/java/com/greenlease/repository/UserRepository.java
+++ src/main/java/com/greenlease/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.greenlease.repository;
-//import
+
+
 import com.greenlease.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
