5a659830338bdb7c8548eeaa2931cc865b83a1fa
Yuvashree R <yuvashree.ra05@gmail.com>
2026-07-23 21:11:21 +0530

    Update PropertyRepository.java

--- src/main/java/com/greenlease/repository/PropertyRepository.java
+++ src/main/java/com/greenlease/repository/PropertyRepository.java
@@ -1,5 +1,5 @@
 package com.greenlease.repository;
-sq2
+
 import com.greenlease.model.Property;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
